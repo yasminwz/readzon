3d6ce4b8a7445de112c7efd4b5bcafb2ae919f98
Yasmin Wichinievski Zimermann <yas.desenv@gmail.com>
2024-08-26 19:06:08 -0300

    Initial Commit

---
+++ src/main/java/com/br/readzon/ReadzonApplication.java
@@ -0,0 +1,13 @@
+package com.br.readzon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadzonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadzonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/readzon/ReadzonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.readzon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadzonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
